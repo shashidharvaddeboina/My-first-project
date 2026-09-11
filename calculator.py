e70062e468fbdeb4830e60ea656568a9107881e1
shashidhar <shashidharvksr@gmail.com>
2026-09-11 13:11:20 -0700

    add simple calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print(" Simple Calculator")
+
+num1 = float(input("enter first number = "))
+num2 = float(input("enter second number = "))
+
+print("addition = ",num1+num2)
+print("subtaction = ",num1-num2)
+print("multiplication = ",num1*num2)
+
+if(num2!=0):
+	print("division = ",num1/num2)
+else:
+	print("cannot divide by zero")
